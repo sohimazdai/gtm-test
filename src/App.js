d9fa9bdb6fe51da3e3ada636204c97a80016559d
Andrey Pankratov <avpankratov@domclick.ru>
2021-10-25 12:03:58 +0300

    Init gtm

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
+import { useCallback, useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [dog, setDog] = useState(null);
+
+  const handleFetchDogClick = useCallback(async () => {
+    try {
+      const response = await (await fetch('https://dog.ceo/api/breeds/image/random')).json();
+
+      setDog(response.message);
+    } catch (e) {
+      console.error(e);
+    }
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+      <p>
+        <button onClick={handleFetchDogClick}>fetch a dog</button>
+      </p>
+      {dog && <img src={dog} />}
     </div>
   );
 }
